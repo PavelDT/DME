5b03cd5f4b1e5697c05642553283fec2fa542a69
paveldt <paveldt2@gmail.com>
2019-11-23 16:41:19 +0000

    Indentation fix

--- src/io/github/paveldt/C_buffer.java
+++ src/io/github/paveldt/C_buffer.java
@@ -3,56 +3,56 @@ package io.github.paveldt;
 import java.util.*;
 
 public class C_buffer {
-	
-    private Vector<Object> data;
-    
-    
-    public C_buffer (){
-	data = new Vector<Object>(); 
-    }
-    
 
-    public int size(){
-	
-	return data.size();
-    }
-    
-
-    public synchronized void saveRequest (String[] r){
-	
-	data.add(r[0]);
-	data.add(r[1]);    
-    }
+	private Vector<Object> data;
 
 
-    public void show(){
-	
-	for (int i=0; i<data.size();i++)
-	    System.out.print(" "+data.get(i)+" ");
-	System.out.println(" ");
-    }
-    
-    
-    public void add(Object o){
-	
-	data.add(o);
-	
-    }
-    
-
-    synchronized public Object  get(){
-	
-	Object o = null; 
-	
-	if (data.size() > 0){
-	    o = data.get(0);
-	    data.remove(0);
+	public C_buffer (){
+		data = new Vector<Object>();
 	}
-	return o;
-    }
-    
-    
-    
+
+
+	public int size(){
+
+		return data.size();
+	}
+
+
+	public synchronized void saveRequest (String[] r){
+
+		data.add(r[0]);
+		data.add(r[1]);
+	}
+
+
+	public void show(){
+
+		for (int i=0; i<data.size();i++)
+			System.out.print(" "+data.get(i)+" ");
+		System.out.println(" ");
+	}
+
+
+	public void add(Object o){
+
+		data.add(o);
+
+	}
+
+
+	synchronized public Object  get(){
+
+		Object o = null;
+
+		if (data.size() > 0){
+			o = data.get(0);
+			data.remove(0);
+		}
+		return o;
+	}
+
+
+
 }
 	
 	
--- src/io/github/paveldt/C_receiver.java
+++ src/io/github/paveldt/C_receiver.java
@@ -1,49 +1,46 @@
+import io.github.paveldt.C_Connection_r;
+import io.github.paveldt.C_buffer;
+
 import java.net.*;
 
+public class C_receiver extends Thread {
 
-public class C_receiver extends Thread{
-    
-    private C_buffer 	 buffer; 
-    private int 		port;
-    
-    private ServerSocket 	s_socket; 
-    private Socket		socketFromNode;
-    
-    private C_Connection_r	connect;
-    
-    
-    public C_receiver (C_buffer b, int p){
-	buffer = b;
-	port = p;
-    }
-    
-    
-    public void run () {
-	
-	// >>> create the socket the server will listen to
+	private C_buffer buffer;
+	private int 		port;
+
+	private ServerSocket 	s_socket;
+	private Socket		socketFromNode;
+
+	private C_Connection_r connect;
 
 
-
-
-	while (true) {
-
-
-	    try{
-	        
-		// >>> get a new connection
-
-		System.out.println ("C:receiver    Coordinator has received a request ...") ;
-		
-		// >>> create a separate thread to service the request, a C_Connection_r thread.
-
-	    }
-	    catch (java.io.IOException e) {
-		System.out.println("Exception when creating a connection "+e);
-	    }
-	    
+	public C_receiver (C_buffer b, int p){
+		buffer = b;
+		port = p;
 	}
-    }//end run
-    
+
+	@Override
+	public void run () {
+
+		// >>> create the socket the server will listen to
+		while (true) {
+
+			try{
+
+				// >>> get a new connection
+
+				System.out.println ("C:receiver    Coordinator has received a request ...") ;
+
+				// >>> create a separate thread to service the request, a C_Connection_r thread.
+
+			}
+			catch (java.io.IOException e) {
+				System.out.println("Exception when creating a connection "+e);
+			}
+
+		}
+	}//end run
+
 }
 	
 
